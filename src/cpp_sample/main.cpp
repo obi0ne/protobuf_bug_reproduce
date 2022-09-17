d7bdac41db958ffb5908efb2c04038738e188ee9
Idan <idanbismut@gmail.com>
2022-09-17 19:54:21 +0300

    support two way testing (csharp <--> cpp)

--- src/cpp_sample/main.cpp
+++ src/cpp_sample/main.cpp
@@ -3,14 +3,18 @@
 #include <fstream>
 #include "test.pb.h"
 
+#define DEBUG_CPP_TO_CSHARP
+
 int main(int argc, char* argv[])
 {
+
+	// Test C++ -> C# .NET direction
+#ifdef DEBUG_CPP_TO_CSHARP
 	auto obj = std::make_shared<test_me::State_Data>();
 	obj->add_myvector(1.0f);
 	obj->add_myvector(1.0f);
 	obj->add_myvector(1.0f);
-	obj->add_myvector(1.0f);
-		
+	obj->add_myvector(1.0f);		
 	std::string buf = obj->SerializeAsString();
 
 	auto myVec = obj->myvector();
@@ -19,11 +23,34 @@ int main(int argc, char* argv[])
 	auto comp3 = myVec[2];
 	auto comp4 = myVec[3];
 
-
 	size_t buf_size = buf.size();
-	std::ofstream outfile("serialized_state_obj.bin", std::ofstream::binary);
+	std::ofstream outfile("cpp_to_csharp_serialized_state_obj.bin", std::ofstream::binary);
 	outfile.write(&buf[0], buf_size);
 	outfile.close();
+
+#else
+
+	// Test C# .NET -> C++ direction
+	std::ifstream infile("csharp_to_cpp_serialized_state_obj.bin", std::ofstream::binary);
+	infile.seekg(0, std::ios::end);
+	auto fileSize = infile.tellg();
+	infile.seekg(0, std::ios::beg);
+	
+	std::vector<BYTE> fileData(fileSize);
+	infile.read((char*)&fileData[0], fileSize);
+	infile.close();
+
+	test_me::State_Data newObj;
+	auto res = newObj.ParseFromArray((char*)&fileData[0], fileSize);
+
+	auto myNewVector = newObj.myvector();
+	auto new_comp1 = myNewVector[0];
+	auto new_comp2 = myNewVector[1];
+	auto new_comp3 = myNewVector[2];
+	auto new_comp4 = myNewVector[3];
+
+
+#endif
 
 	return 0;
 }
--- src/csharp_sample/main.cs
+++ src/csharp_sample/main.cs
@@ -1,3 +1,5 @@
+#define DEBUG_CPP_TO_CSHARP
+
 using System.IO;
 using Google.Protobuf;
 using TestMe;
@@ -8,7 +10,10 @@ namespace bug_reproduce
     {
         static void Main(string[] args)
         {
-            using (var fileStream = File.OpenRead("serialized_state_obj.bin"))
+
+#if DEBUG_CPP_TO_CSHARP
+            // Check deserializatin of data sent from C++ -> C# .NET
+            using (var fileStream = File.OpenRead("cpp_to_csharp_serialized_state_obj.bin"))
             {
                 byte[] buf = new byte[fileStream.Length];
                 fileStream.Read(buf, 0, (int)fileStream.Length);
@@ -20,11 +25,28 @@ namespace bug_reproduce
                 var comp2 = myVector[1];
                 var comp3 = myVector[2];
                 var comp4 = myVector[3];
-
-
             }
 
-            // Code in here !
+#else
+
+            // Check serialization of data sent from C# -> C++
+
+            State_Data newObj = new TestMe.State_Data();
+            var myNewVector = newObj.MyVector;
+            myNewVector.Add(1.0f);
+            myNewVector.Add(1.0f);
+            myNewVector.Add(1.0f);
+            myNewVector.Add(1.0f);
+
+            var newStateBuf = newObj.ToByteArray();
+            using (var fileStream = File.OpenWrite("csharp_to_cpp_serialized_state_obj.bin"))
+            {
+                fileStream.Write(newStateBuf, 0, newStateBuf.Length);
+                fileStream.Close();
+            }           
+
+#endif
+                        
         }
 
     }
